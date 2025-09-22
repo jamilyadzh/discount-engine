c284ca9511e376ba5678fb393eb00417940c8499
“Jamilya <dzhumabekova@usf.edu>
2025-09-21 22:49:05 -0400

    step 3

--- cc_4.js
+++ cc_4.js
@@ -11,3 +11,23 @@ let products = [
 
 console.log("Initial products:", products);
 
+// apply category-based discount using switch
+// switch lets us branch by product category. use break to prevent fallthrough
+for (let product of products) {
+  switch (product.category) {
+    case "electronics":
+      product.price *= 0.8; // 20% off
+      break;
+    case "apparel":
+      product.price *= 0.85; // 15% off
+      break;
+    case "groceries":
+    case "household":
+      product.price *= 0.9; // 10% off
+      break;
+    default:
+      // No discount
+      break;
+  }
+}
+console.log("After category discounts:", products);
